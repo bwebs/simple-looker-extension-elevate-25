fec3b4c51cba72e1550ec0d4a4b154d3de19025c
bwebs <bfweber@gmail.com>
2025-06-09 15:30:44 -0700

    Add GitHub Actions workflow for deploying to GitHub Pages, update deployment branch in config, and modify deploy script to use SSH

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -21,6 +21,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: isDev ? "/" : "/simple-looker-extension-elevate-25/",
+  deploymentBranch: "gh-pages",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
